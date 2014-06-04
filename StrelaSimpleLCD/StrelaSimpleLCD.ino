7c78ac5e77e4a59bdfc2ec9d8370fafd9c53bcb5
acosinwork <acosinwork@yandex.ru>
2014-06-04 18:44:40 +0400

    Update StrelaSimpleLCD.ino

--- StrelaSimpleLCD/StrelaSimpleLCD.ino
+++ StrelaSimpleLCD/StrelaSimpleLCD.ino
@@ -8,20 +8,44 @@
 //                    addr, en,rw,rs,d4,d5,d6,d7,bl,blpol
 LiquidCrystal_I2C lcd(LC_ADDR, LCEN, LCRW, LCRS, LC4, LC5, LC6, LC7);
 
-Strela S;
 
 void setup()
 {
 
   Wire.begin();
-  S.begin();
   lcd.begin(8, 2);
+  
+  uPinMode(P10, INPUT);
+  uPinMode(P9, INPUT);
 
 }
 
 void loop()
 {
   
+  
+
+  lcd.clear();
+  lcd.home();
+  
+  lcd.print(uDigitalRead(P10));
+  lcd.print(uDigitalRead(P9));
+  
+  lcd.setCursor(0, 1);
+  lcd.print(uDigitalRead(S1));
+  lcd.print(uDigitalRead(S2));
+  lcd.print(uDigitalRead(S3));
+  lcd.print(uDigitalRead(S4));
+  
+  
+  delay(30);
+  
+  if (uDigitalRead(S1))
+  uDigitalWrite(L1, LOW);
+  else
+  uDigitalWrite(L1, HIGH);
+  
+  /*
   for (int i=L4; i<=L1; ++i)
   {
       lcd.clear();
@@ -32,7 +56,7 @@ void loop()
     uDigitalWrite(i, LOW);
     delay(1000);
   }
-
+*/
 
 }
 
