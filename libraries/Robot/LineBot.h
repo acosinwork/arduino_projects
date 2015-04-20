17d69ab612d53b01c32a085dd79306d330fb100f
acosinwork <acosinwork@yandex.ru>
2015-04-20 19:46:24 +0300

    LineLinLine

---
+++ libraries/Robot/LineBot.h
@@ -0,0 +1,9 @@
+#ifndef LINEBOT_H
+#define LINEBOT_H
+
+#include <inttypes.h>
+#include <Robot.h>
+
+
+
+#endif
--- libraries/Robot/Robot.h
+++ libraries/Robot/Robot.h
@@ -25,6 +25,7 @@ class Robot {
 //		virtual void run();
 
 //	protected:
+
 		void begin();
 
     private:
